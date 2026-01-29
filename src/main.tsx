4f1bda0b94fe2df2e30290f310922253447fb0e9
Alvaro Videla <videlalvaro@gmail.com>
2026-01-29 23:22:38 +0100

    Disable StrictMode to fix Cytoscape compatibility
    React 18 StrictMode double-mounts components in development,
    which causes Cytoscape.js internal errors when the first instance
    is destroyed while the second is initializing.
    
    StrictMode does not affect production builds.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+// Note: StrictMode disabled due to Cytoscape.js incompatibility with double-mounting
+// This only affects development - production builds don't use StrictMode anyway
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />,
 )
